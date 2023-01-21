4100cfcfc85a0cfa81d9145a09bffbffe394379b
SaveAsArtaz <artaz.for.git@gmail.com>
2023-01-22 02:35:53 +0330

    avoidance method by using semaphore for controlling mutex

---
+++ os_concurrency/RightSide.py
@@ -0,0 +1,30 @@
+from multiprocessing import  Process, Queue, Value,Array
+from time import sleep
+from Car import Car
+
+
+
+def producer(queue, id, s1):
+    print('Producer: Running', flush=True)
+    while True:
+        s1.Wait(0)
+        value = Car(id.value)
+        id.value += 1
+        sleep(0.5)
+        queue.put(value)
+        s1.Signal()
+
+
+def consumer(queue, street, s2):
+    print('Consumer: Running', flush=True)
+    while True:
+        s2.Wait(0)
+        item = queue.get()
+        street.value = item.id
+        print('car id: ', item.id, 'sleep: ', item.time)
+        temp = street.value
+        sleep(item.time)
+        if temp != street.value:
+            print('Process conflict!')
+        s2.Signal()
+        street.value = 0
